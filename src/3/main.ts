5346df557f47a35ee8be5e7b97e452fad5a69c45
Federico Saglia <federico.saglia@gmail.com>
2022-12-04 12:04:39 +0100

    day3: part-two

--- src/3/main.ts
+++ src/3/main.ts
@@ -2,24 +2,60 @@ import { readFileByLine } from "../utils";
 
 const rucksackList = readFileByLine(`${__dirname}/input.txt`);
 
-// console.log(getPriority("a"));
-getPriority("A");
-getPriority("Z");
+const prioritiesPartOne: number[] = rucksackList.map((rucksack) => {
+  // last line might be an empty (can be considered as empty rucksack)
+  if (rucksack.length === 0) return 0;
+  return getPriority(getSharedItem(rucksack));
+});
 
-const priorities = rucksackList
-  .map((rucksack) => {
-    const sharedLetterList = getSharedItems(rucksack);
-    const priorityList = sharedLetterList.map(getPriority);
-    const prioritySum = priorityList.reduce((acc, item) => acc + item, 0);
-    return prioritySum;
-  })
-  .reduce((acc, item) => acc + item, 0);
+const prioritiesPartTwo: number[] = [];
+for (let i = 0; i < rucksackList.length; i += 3) {
+  const group = rucksackList.slice(i, i + 3);
+  if (group.length > 1) {
+    const sharedItem = getSharedItemGroup(group);
+    prioritiesPartTwo.push(getPriority(sharedItem));
+  }
+}
 
-console.log(`Sum o priorities: ${priorities}`);
+console.log(`[Part one] Sum: ${sumArrayValues(prioritiesPartOne)}`);
+console.log(`[Part two] Sum: ${sumArrayValues(prioritiesPartTwo)}`);
 
-function getSharedItems(rucksack: string): string[] {
+/**
+ * Helpers
+ */
+
+function sumArrayValues(array: number[]): number {
+  return array.reduce((acc, item) => acc + item, 0);
+}
+
+function getSharedItemGroup(rucksackList: string[]): string {
+  const itemCounterMap: { [letter: string]: number } = {};
+  const rucksackListUnique = rucksackList
+    .map((rucksack) => [...new Set(Array.from(rucksack))])
+    .reduce((acc, item) => [...acc, ...item], []);
+
+  for (let i = 0; i < rucksackListUnique.length; i++) {
+    const item = rucksackListUnique[i];
+    const count = itemCounterMap[item] || 0;
+    itemCounterMap[item] = count + 1;
+  }
+
+  let max = { letter: "", count: 0 };
+
+  for (const letter of Object.keys(itemCounterMap)) {
+    const count = itemCounterMap[letter];
+    if (count > max.count) max = { letter, count };
+  }
+
+  console.log(max, rucksackList);
+  if (max.count !== rucksackList.length)
+    throw Error(`No shared letters found: ${rucksackList}`);
+  return max.letter;
+}
+
+function getSharedItem(rucksack: string): string {
   const itemsCount = rucksack.length;
-  if (itemsCount === 0) return [];
+  if (itemsCount === 0) throw Error("Error: emtpy rucksack!");
 
   const compartmentOneMap: { [k: string]: string } = {};
   const compartmentTwoMap: { [k: string]: string } = {};
@@ -31,9 +67,13 @@ function getSharedItems(rucksack: string): string[] {
     compartmentTwoMap[itemTwo] = itemTwo;
   }
 
-  return Object.keys(compartmentOneMap).filter((k) =>
+  const sharedList = Object.keys(compartmentOneMap).filter((k) =>
     compartmentTwoMap.hasOwnProperty(k)
   );
+
+  if (sharedList.length !== 1)
+    throw Error(`Multiple shared items found: ${sharedList}`);
+  return sharedList[0];
 }
 
 function getPriority(letter: string): number {
