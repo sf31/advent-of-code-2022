9c2158909949fd6253b889df27cb67476cb1c1d4
Federico Saglia <federico.saglia@gmail.com>
2022-12-06 16:21:25 +0100

    day6: part-two

--- src/6/main.ts
+++ src/6/main.ts
@@ -1,15 +1,29 @@
 import { readFileByLine } from "../utils";
 
 const file = readFileByLine(`${__dirname}/input.txt`)[0];
-const index = getMarkerIndex(file);
-const output = index === -1 ? "Marker not found" : `Found Marker at: ${index}`;
-console.log(output);
 
-function getMarkerIndex(buffer: string): number {
-  for (let i = 3; i < buffer.length; i++) {
-    const subset = buffer.slice(i - 4, i);
+const startOfPacketIndex = getMarkerIndexIndex(file, 4);
+console.log(
+  startOfPacketIndex === -1
+    ? "Start-of-packet not found"
+    : `Start-of-packet found at: ${startOfPacketIndex}`
+);
+
+const startOfMessagetIndex = getMarkerIndexIndex(file, 14);
+console.log(
+  startOfMessagetIndex === -1
+    ? "Start-of-message not found"
+    : `Start-of-message found at: ${startOfMessagetIndex}`
+);
+
+/**
+ * Pass 4 for start-of-packet, 14 for start-of-message
+ */
+function getMarkerIndexIndex(buffer: string, windowSize: number): number {
+  for (let i = windowSize - 1; i < buffer.length; i++) {
+    const subset = buffer.slice(i - windowSize, i);
     const distinctCharCount = new Set(Array.from(subset)).size;
-    if (distinctCharCount === 4) return i;
+    if (distinctCharCount === windowSize) return i;
   }
   return -1;
 }
