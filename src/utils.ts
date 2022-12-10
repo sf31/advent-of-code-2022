92612d7405b17f8e5c6d156cede00a44b1cc186f
Federico Saglia <federico.saglia@gmail.com>
2022-12-10 19:42:11 +0100

    refactor: skip empty lines while parsing input

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,15 @@
 import * as fs from "fs";
 
-export function readFileByLine(path: string): string[] {
+export function readFileByLine(
+  path: string,
+  skipEmptyLines: boolean = false
+): string[] {
+  return !skipEmptyLines
+    ? readFile(path)
+    : readFile(path).filter((line) => line.length);
+}
+
+function readFile(path: string): string[] {
   return fs.readFileSync(path, "utf8").toString().split("\n");
 }
 
